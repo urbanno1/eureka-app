a2e05024fca1072423132540d196b318a53f1ccd
urbanno1 <eduurbanus42@gmail.com>
2020-08-29 23:49:57 -0400

    added spring cloud ribbon for load balancing using round robbin algorithms

--- AccountService/src/main/java/com/monycell/edu/accountservice/controller/AccountController1.java
+++ AccountService/src/main/java/com/monycell/edu/accountservice/controller/AccountController1.java
@@ -1,6 +1,7 @@
 package com.monycell.edu.accountservice.controller;
 
 import com.monycell.edu.accountservice.model.Account;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AccountController {
+@Profile("One")
+public class AccountController1 {
 
     @RequestMapping("/account/{customerId}")
     public ResponseEntity<Account> getAccount(@PathVariable("customerId") String customerId) {
+        System.out.println("Account controller1 called");
+
         Account account = new Account(customerId, 35000.00);
         return new ResponseEntity<>(account, HttpStatus.OK);
     }
